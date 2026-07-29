abf9d3c235a93e21f657650dfffbda80474af4d1
sebastiansabo <sebastian.sabo@gmail.com>
2026-07-29 12:29:03 +0300

    feat(organization): is_manager reads Sincron organigram responsables (+ L0)
    (cherry picked from commit afe67945dd295c97b8586f8eeed57e7e9484bb38)

--- jarvis/tests/org/test_manager_utils_sincron.py
+++ jarvis/tests/org/test_manager_utils_sincron.py
@@ -3,6 +3,8 @@ import os
 os.environ.setdefault('DATABASE_URL', 'postgresql://localhost/defaultdb')
 import psycopg2  # noqa: F401
 
+from core.organization.manager_utils import is_manager, get_managed_employee_ids, get_visible_tree
+
 
 def test_fixture_seeds_expected_topology(org_fixture):
     from database import get_db, get_cursor, release_db
@@ -15,3 +17,16 @@ def test_fixture_seeds_expected_topology(org_fixture):
         assert cur.fetchone()['n'] == 6
     finally:
         release_db(conn)
+
+
+def test_is_manager_sincron_responsable(org_fixture):
+    assert is_manager(org_fixture['user_M']) is True   # responsable @ P
+
+
+def test_is_manager_l0(org_fixture):
+    assert is_manager(org_fixture['user_L0']) is True   # company_responsables
+
+
+def test_is_manager_plain_member_false(org_fixture):
+    assert is_manager(org_fixture['user_A']) is False   # only a member
+    assert is_manager(org_fixture['user_X']) is False   # in company, no org node
